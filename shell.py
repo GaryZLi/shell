a05536c1d91c53a64c00e89c9c3a81e9b8b159b0
GaryZLi <gary.zecun@gmail.com>
2020-02-05 19:22:48 -0800

    Fdsfds

--- shell.py
+++ shell.py
@@ -35,27 +35,60 @@ class Process:
     def init(self):
         # self.head = []
         # self.head.append(Node(0))
-        self.PCB = [0]*16
+        self.PCB = [[]]*16
         self.RCB = [1,1,2,3]
-        self.RL = []
+        self.RL = [[], []]
         self.currentProcess = 0
-        self.PCB[0] = 1
+        self.PCB[0] = [0]
 
         print("process 0 created")
         self.write(0)
 
     def create(self, priority):
-        with open("testoutput.txt", "a+") as file:
-            file.write(str(self.currentProcess) + " ")
-        
+        with open("testoutput.txt", "a+") as file:                
+            if priority == 0 or self.PCB == None:
+                file.write(-1)
+                return
+
+            # file.write(str(self.currentProcess) + " ")
+
+        # get the next free process
         for i in range(0, len(self.PCB)):
-            if self.PCB[i] == 0:
-                self.PCB[i] = 1
+            if len(self.PCB[i]) == 0:
+                self.PCB[i] = [self.currentProcess]
                 break
+
+        self.PCB[self.currentProcess].append(i) # add the process
+        self.RL[priority-1].append(i) # add to ready list
         print("process %d created" % i)
 
-    def destroy(self, process):
-        pass
+        for i in self.RL:
+            if len(i) > 0:
+                self.currentProcess = i[0]
+                break
+
+        with open("testoutput.txt", "a+") as file:
+            file.write(str(self.currentProcess) + ' ')
+
+    def destroy(self, process, total = 0): #can process 0 be destroyed?
+        # if process == 0:
+        #     total += len(self.PCB[0]) - 1
+        #     return total
+
+        # IM BASICALLY DESTROYING PROCESS 0, AND THIS SKIPS BECUZ LEN(PROCESS 0) > 1
+        if len(self.PCB[process]) == 1:
+            self.PCB[self.PCB[process].pop()].pop(1)
+            return total + 1
+
+
+        while len(self.PCB[process]) > 0:
+            total += 1 
+            self.destroy(self.PCB[process][0], total)
+
+        # self.destroy(self.PCB[process][0])
+        self.PCB[self.PCB[process].pop()].pop(1) #need to pop itself and everything after itself in its parent
+
+        return total + 1
 
     def request(self, resource, units): #has sched
         pass
@@ -78,9 +111,9 @@ class Process:
                     if line[0] == "in":
                         self.init()
                     elif line[0] == "cr":
-                        self.create(4)
-                    # elif line[0] == "de":
-                    #     destroy()
+                        self.create(int(line[1]))
+                    elif line[0] == "de":
+                        print(self.destroy(int(line[1])), "process destroyed")
                     # elif line[0] == "rq":
                     #     request()
                     # elif line[0] == "rl":
@@ -88,5 +121,14 @@ class Process:
                     # elif line[0] == "to":
                     #     timeout()
 
+        print(self.PCB)
+
 run = Process() 
 run.shell()
+
+# a = [[], [10], [11, 2]]
+
+
+# for x in a:
+#     for y in x:
+#         print(y)
